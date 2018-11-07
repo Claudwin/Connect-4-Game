775afd7beae2ef26fcd49d05665ad456a1e4d0e5
Claudwin Fortune <Claudwin@users.noreply.github.com>
2018-11-06 17:33:03 -0700

    Built board.js which generates 2D array of spaces.

--- Stage1 - Introducing Four in a Row/Stage1Step1 - Introducing the Project/js/Board.js
+++ Stage1 - Introducing Four in a Row/Stage1Step1 - Introducing the Project/js/Board.js
@@ -1,7 +1,26 @@
+
 class Board {
     constructor() {
         this.row = 6;
         this.column = 7;
-        this.space = [];
+        this.space = this.createSpaces();
+        /** 
+ * Generates 2D array of spaces. 
+ * @return  {Array}     An array of space objects
+ */
+        createSpaces() {
+            const spaces = [];
+            for (let x = 0; x < this.column; x++) { //iterates through the number of columns 
+                const column = []; //Holds the individual space objects in column
+
+            for (let y = 0; y < this.row; y++) { //Fills the spaces in each column 
+                const space = new Space(x,y);       //spaces-array is a two dimensional array
+                column.push(space);                 //Pushes spaces to column array
+            }
+                spaces.push(column);                //Pushes columns with space to the spaces array
+
+            }
+                return spaces;
+        }
     }
 }
